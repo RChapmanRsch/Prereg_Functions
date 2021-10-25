432283fbc51b235e8b101861f1e10982eac133e5
zenit125 <robertchapman125@gmail.com>
2021-10-24 20:14:06 -0500

    Update Missing_Data.R

--- Missing_Data.R
+++ Missing_Data.R
@@ -8,8 +8,8 @@ Missing_Patterns = function(Data){
   return(aggregate(list(Freq=rep(1,nrow(Missing_Data_Rows))), Missing_Data_Rows, length))}}
 
 #for pre-running & Example only:
-Missing_Subset=Data[,c("Age","Gender",PROMIS_Variables$Depression)]
-for(i in 3:6){Missing_Subset[,i]=as.factor(sample(c(NA,1:5), 2000, replace=TRUE))}
+#Missing_Subset=Data[,c("Age","Gender",PROMIS_Variables$Depression)]
+#for(i in 3:6){Missing_Subset[,i]=as.factor(sample(c(NA,1:5), 2000, replace=TRUE))}
 
 
 # Little's MNAR test & demo/remaining relationship (missing-item/scale)
@@ -21,10 +21,10 @@ mcar(Missing_Subset[,PROMIS_Variables$Depression])
 
 
 #for pre-running & Example only - dichotomize missing data:
-Missing_Subset=Data[,c("Age","Gender",PROMIS_Variables$Depression)]
-for(i in 3:5){Missing_Subset[,i]=as.factor(sample(c(1,0), 2000, prob=c(0.1,0.9), replace=TRUE))}
+#Missing_Subset=Data[,c("Age","Gender",PROMIS_Variables$Depression)]
+#for(i in 3:5){Missing_Subset[,i]=as.factor(sample(c(1,0), 2000, prob=c(0.1,0.9), replace=TRUE))}
 
 
 #T-Test and Regression for looking at differences between missing data (dichotomized)
-t.test(Gender~EDDEP04, data=Missing_Subset, var.equal=FALSE)
-summary(lm(Age~EDDEP04, data=Missing_Subset))
+#t.test(Gender~EDDEP04, data=Missing_Subset, var.equal=FALSE)
+#summary(lm(Age~EDDEP04, data=Missing_Subset))
